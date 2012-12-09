e112d6306bfd4d93c3f2b1ec4efc6cf459769331
Ritchie Cai <ritchiecai@r2labs.org>
2012-12-08 16:41:26 -0800

    print input in arranged form

--- Zombie_March/zombie_march.lisp
+++ Zombie_March/zombie_march.lisp
@@ -13,6 +13,32 @@
               (setq index next))
             (return (nreverse numbers)))))))
 
+(defun print-table (table)
+  (loop :for key :being the :hash-key of table :using (hash-value value)
+     :do (progn
+           (format t "~a  :  " key)
+           (format t "zombies : ~a " (getf value :zombies))
+           (format t "neighbors : ~a~%" (getf value :neighbors))
+           (format t "      channels : ~%")
+           (loop :for k :being the :hash-key of (getf value :channel) :using (hash-value v)
+              :do (format t "            [~a : ~a] ~%" k v)))))
+
+(defun setup-channels (table)
+  (loop
+     :for key :being the :hash-key of table :using (hash-value value)
+     :do (symbol-macrolet ((neighbors (getf value :Neighbors))
+                           (zombies   (getf value :zombies)))
+           (loop
+              :with count = (length neighbors)
+              :for i from 1 to zombies
+              :do (loop :for n :in neighbors
+                     :do (symbol-macrolet ((channel (getf (gethash n table) :channel)))
+                           (unless channel
+                             (setf channel (make-hash-table))
+                             (setf (gethash :count channel) 0)) 
+                           (push (/ 1 count) (gethash key channel))
+                           (incf (gethash :count channel)))))))) 
+
 (defun get-input (filename)
   (with-open-file (in filename :direction :input)
     ;; ignore the first line for now
@@ -21,12 +47,24 @@
         ((= test-count 0))
       (destructuring-bind (nodes roads steps)
           (read-numbers-from-string (read-line in nil))
-        (format t "~%nodes=~a~%roads=~a~%steps=~a~%" nodes roads steps)
-        (print "edges:")
-        (do ((counter 1 (incf counter)))
-            ((> counter roads))
-          (print (read-numbers-from-string (read-line in nil))))
-        (print "vertices:")
-        (do ((counter 1 (incf counter)))
-            ((> counter nodes))
-          (print (read-numbers-from-string (read-line in nil))))))))
+        (let ((junctions (make-hash-table :test #'equal :size nodes)))
+          (format t "~%nodes=~a~%roads=~a~%steps=~a~%" nodes roads steps)
+          ;; (print "edges:")
+          (do ((counter 1 (incf counter)))
+              ((> counter roads))
+            (let ((pair (read-numbers-from-string (read-line in nil))))
+              ;;(print pair)
+              (push (second pair) (getf (gethash (first pair)  junctions)
+                                        :Neighbors))
+              (push (first pair)  (getf (gethash (second pair) junctions)
+                                        :Neighbors))))
+
+          ;; (princ "vertices:")
+          (do ((counter 0 (incf counter)))
+              ((>= counter nodes))
+            (setf (getf (gethash counter junctions) :zombies)
+                  (read-from-string (read-line in nil))))
+
+          (format t "~%Table~%")
+          (setup-channels junctions)
+          (print-table    junctions))))))
