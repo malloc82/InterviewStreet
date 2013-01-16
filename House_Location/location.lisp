f9184413af9182c098214b50b33f036ea7a46383
Ritchie Cai <ritchiecai@r2labs.org>
2013-01-15 22:10:00 -0800

    eval-formula fix

--- House_Location/location.lisp
+++ House_Location/location.lisp
@@ -45,67 +45,74 @@
        `(sqrt (eval-formula ,(second expr))))
       ((equal (first expr) 'expt)
        `(expt (eval-formula ,(second expr)) (eval-formula ,(third expr))))
-      ((= (length expr) 1)
-       `(eval-formula ,(first expr)))
-      (t (loop
-            :for (token op) :on expr :by #'cddr
-            :if (null op)
-            :do (return
-                  ;; can do one call with more than two arguments if possible
-                  (loop
-                     :initially (push `(eval-formula ,token) operands)
-                     :while operators
-                     :do (push (loop :with result = `(,(pop operators) ,(pop operands) ,(pop operands))
-                                  :while (equal (first operators) (first result))
-                                  :do (progn
-                                        (pop operators)
-                                        (nconc result (list (pop operands))))
-                                  :finally (return result))
-                               operands)
-                     :finally (return (first operands)))
+      ;; ((and (listp expr) (= (length expr) 1))
+      ;;  `(eval-formula ,(first expr)))
+      ((or (numberp (first expr))
+           (listp (first expr)))
+       (loop
+          :for (token op) :on expr :by #'cddr
+          :if (null op)
+          :do (return
+                ;; can do one call with more than two arguments if possible
+                (loop
+                   :initially (push `(eval-formula ,token) operands)
+                   :while operators
+                   :do (push (loop :with result = `(,(pop operators) ,(pop operands) ,(pop operands))
+                                :while (equal (first operators) (first result))
+                                :do (progn
+                                      (pop operators)
+                                      (nconc result (list (pop operands))))
+                                :finally (return result))
+                             operands)
+                   :finally (return (first operands)))
 
-                  ;; ;; Work but will not take advantage that op functions can take
-                  ;; ;; more than two arguments
-                  ;; (loop
-                  ;;    :initially (push token operands)
-                  ;;    :for op :in operators
-                  ;;    :do (push `(,(pop operators) ,(pop operands) ,(pop operands)) operands)
-                  ;;    :finally (return (first operands)))
-                  )
-            :do (progn
-                  (push `(eval-formula ,token) operands)
-                  (if (null operators)
-                      (push op operators)
-                      (loop :for group :in precedence
-                          :do (let ((incoming (member op group))
-                                    (top      (member (first operators) group)))
-                                (cond
-                                  (incoming
-                                   (push op operators)
-                                   (return))
-                                  ((and (not incoming) top)
-                                   (push (list (pop operators)
-                                               `(eval-formula ,(pop operands))
-                                               `(eval-formula ,(pop operands))) operands)
-                                   (push op operators)
-                                   (return))))))))))))
+                ;; ;; Work but will not take advantage that op functions can take
+                ;; ;; more than two arguments
+                ;; (loop
+                ;;    :initially (push token operands)
+                ;;    :for op :in operators
+                ;;    :do (push `(,(pop operators) ,(pop operands) ,(pop operands)) operands)
+                ;;    :finally (return (first operands)))
+                )
+          :do (progn
+                (push `(eval-formula ,token) operands)
+                (if (null operators)
+                    (push op operators)
+                    (loop :for group :in precedence
+                       :do (let ((incoming (member op group))
+                                 (top      (member (first operators) group)))
+                             (cond
+                               (incoming
+                                (push op operators)
+                                (return))
+                               ((and (not incoming) top)
+                                (push (list (pop operators)
+                                            `(eval-formula ,(pop operands))
+                                            `(eval-formula ,(pop operands))) operands)
+                                (push op operators)
+                                (return)))))))))
+      (t expr))))
 
 
 (defun calculate-location (a b kimberly bob jack janet)
-  (let* ((1/a^2 (/ 1 (expt a 2)))
-         (1/b^2 (/ 1 (expt b 2)))
-         (Pax (+ (- 1/a^2 1) (- 1/b^2 1)))
-         (Pbx (* 2 (+ (- (location-x bob) (* (location-x kimberly) 1/a^2))
-                      (- (location-x janet) (* (location-x jack) 1/b^2)))))
-         (Pcx (+ (- (* (expt (location-x kimberly) 2) 1/a^2) (expt (location-x bob) 2))
-                 (- (* (expt (location-x jack) 2) 1/b^2) (expt (location-x janet) 2))))
-         (Pay (+ (- 1/a^2 1) (- 1/b^2 1)))
-         (Pby (* 2 (+ (- (location-y bob)   (* (location-y kimberly) 1/a^2))
-                      (- (location-y janet) (* (location-y jack)     1/b^2)))))
-         (Pcy (+ (- (* (expt (location-y kimberly) 2) 1/a^2) (expt (location-y bob) 2))
-                 (- (* (expt (location-y jack) 2) 1/b^2) (expt (location-y janet) 2))))
-         (Kx (- (/ (expt Pbx 2) (* 4 Pax)) Pcx))
-         (Ky (- (/ (expt Pby 2) (* 4 Pay)) Pcy)))
+  (let* ((1/a^2 (eval-formula (1 / (expt a 2))))
+         (1/b^2 (eval-formula (1 / (expt b 2))))
+
+         (Pax   (eval-formula ((1/a^2 + 1) + (1/b^2 + 1))))
+         (Pbx   (eval-formula ((2 * (location-x bob)   - 2 * (location-x kimberly) * 1/a^2) +
+                               (2 * (location-x janet) - 2 * (location-x jack) * 1/b^2))))
+         (Pcx   (eval-formula (((expt (location-x kimberly) 2) * 1/a^2 - (expt (location-x bob) 2)) +
+                               ((expt (location-x jack) 2) * 1/b^2 - (expt (location-x janet) 2)))))
+
+         (Pay   (eval-formula ((1/a^2 - 1) + (1/b^2 - 1))))
+         (Pby   (eval-formula ((2 * (location-y bob) - 2 * (location-y kimberly) * 1/a^2) +
+                               (2 * (location-y janet) - 2 * (location-y jack) * 1/b^2))))
+         (Pcy   (eval-formula (((expt (location-y kimberly) 2) * 1/a^2 - (expt (location-y bob) 2)) +
+                               ((expt (location-y jack) 2) * 1/b^2 - (expt (location-y janet) 2)))))
+
+         (Kx    (eval-formula (((expt Pbx 2) / (4 * Pax)) - Pcx)))
+         (Ky    (eval-formula (((expt Pby 2) / (4 * Pay)) - Pcy))))
+    
     (format *standard-output* "X : a=~a, b=~a, c=~a~%" Pax Pbx Pcx)
     (format *standard-output* "Y : a=~a, b=~a, c=~a~%" Pay Pby Pcy)
     (format *standard-output* "Kx = ~a~%" Kx)
